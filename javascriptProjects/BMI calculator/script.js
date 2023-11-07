132616ba551c377b282d3cb864cddb7f74323eb9
ms-megha <147719126+ms-megha@users.noreply.github.com>
2023-11-07 13:11:00 +0000

    bmi calculator

--- javascriptProjects/BMI calculator/script.js
+++ javascriptProjects/BMI calculator/script.js
@@ -1,11 +1,11 @@
-
+const underWeight = document.querySelector('#under-weight').innerHTML
+const normalRenge = document.querySelector('#normal-renge').innerHTML
+const overWeight = document.querySelector('#overweight').innerHTML
 const form = document.querySelector('form')
 form.addEventListener('submit', function(e){
     e.preventDefault();
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
-    const calculate = document.querySelector('#calculate')
-    const reset = document.querySelector('#reset')
     const result = document.querySelector('.result')
     if(height==='' || height<0 || isNaN(height)){
         result.innerHTML = `Please enter a valid height ${height}`
@@ -15,11 +15,14 @@ form.addEventListener('submit', function(e){
     else{
         const bmi = (weight / ((height * height)/10000).toFixed(2))
         if(bmi <18.6){
-            result.innerHTML=`BMI is : ${bmi} and `
+            result.innerHTML=`BMI is : ${bmi} and you are in ${underWeight}`
+        }else if(bmi>=18.6 && bmi<=24.9){
+            result.innerHTML = `BMI is : ${bmi} and you are in ${normalRenge}`
         }
-        
+        else{
+            result.innerHTML = `BMI is : ${bmi} and you are in ${overWeight}`
+        }
     }
 
 
 })
-
