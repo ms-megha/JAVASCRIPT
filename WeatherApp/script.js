aad3ab358d85864b320c6e33929bc531b71bda16
ms-megha <147719126+ms-megha@users.noreply.github.com>
2024-01-06 06:52:11 +0000

    weather app

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -5,16 +5,40 @@ const searchBtn = document.querySelector("#search-btn");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    if (!response.ok) {
-        alert("City not found. Please enter a valid city name.");
-        return;
-    }
     let data = await response.json();
-    let weatherImg = document.querySelector(".weather-img");
+    console.log(data);
+    let weatherImg = document.querySelector(".weather-img")
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + '°C';
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".humidity").innerHTML = data.main.humidity + '%';
     document.querySelector(".wind-speed").innerHTML = data.wind.speed + ' km/h';
+    if(data.weather[0].main == "Clear"){
+        weatherImg.src = "images/clear.png"
+    }else{
+        if(data.weather[0].main == "Clouds"){
+            weatherImg.src="images/clouds.png"
+        }
+        else{
+            if(data.weather[0].main == "Drizzle"){
+                weatherImg.src = "images/drizzle.png"
+            }
+            else{
+                if(data.weather[0].main=="Mist"){
+                    weatherImg.src = "images/mist.png"
+                }
+                else{
+                    if(data.weather[0].main == "Rain"){
+                        weatherImg.src="images/rain.png"
+                    }
+                    else{
+                        if(data.weather[0].main == "Snow"){
+                            weatherImg.src = "images/snow.png"
+                        }
+                    }
+                }
+            }
+        }
+    }
     
 }
 
